c3355eaa348c840e5dd9deb1cfde9228dce252ca
Frederico Ferreira <frederico.ferreira@softbox.com.br>
2017-09-22 09:19:59 -0300

    Alteracao icones voltar

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.inappbrowser;
+package br.com.softbox.inappbrowser;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
