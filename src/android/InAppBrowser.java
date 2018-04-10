90ca8c0d0aecad7d787f55c1bfc22b6ac8d07d74
Frederico Ferreira <frederico.dida1@gmail.com>
2018-04-10 17:19:38 -0300

    Forçando download arquivo android

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -506,7 +506,18 @@ public class InAppBrowser extends CordovaPlugin {
         } else {
             this.inAppWebView.loadUrl(url);
         }
-        this.inAppWebView.requestFocus();
+           
+        this.inAppWebView.setDownloadListener(new DownloadListener() {
+            public void onDownloadStart(String url, String userAgent,
+                    String contentDisposition, String mimetype,
+                    long contentLength) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                cordova.getActivity().startActivity(i);
+            }
+         });
+           
+         this.inAppWebView.requestFocus();
     }
 
 
